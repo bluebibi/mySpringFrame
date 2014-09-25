47980ae357dd0ba1260b847a48538951be920afa
Youn-Hee Han <yh21.han@gmail.com>
2014-09-25 13:55:01 +0900

    no message

--- src/main/java/book30/ch03/_5/_2/dao/UserDao.java
+++ src/main/java/book30/ch03/_5/_2/dao/UserDao.java
@@ -83,17 +83,17 @@ public class UserDao {
 	
 	public void add(final User user) throws ClassNotFoundException, SQLException {
 		this.jdbcContext.workWithStatementStrategy(
-				new StatementStrategy() {
-					public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-						PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
-						ps.setString(1, user.getId());
-						ps.setString(2, user.getName());
-						ps.setString(3, user.getPassword());
+			new StatementStrategy() {
+				public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+					PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+					ps.setString(1, user.getId());
+					ps.setString(2, user.getName());
+					ps.setString(3, user.getPassword());
 
-						return ps;
-					}
+					return ps;
 				}
-				);
+			}
+		);
 	}
 	
 	public void deleteAll() throws SQLException {
